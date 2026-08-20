3907b459c889303e7c8f00b75639f5909cba953d
academiaarcana <suporte.academiaarcana@gmail.com>
2026-08-20 15:54:02 -0400

    Conecta frontend ao Supabase do Território Vivo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TERRITORIO_VIVO_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: ''
+  supabaseUrl: 'https://wguurbmtoofkubdawzzr.supabase.co',
+  supabasePublishableKey: 'sb_publishable_rQHwNahb5qL7gkXa0JVXow_fxws-PLY'
 };
