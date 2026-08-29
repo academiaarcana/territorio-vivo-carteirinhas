aa701e7163b5761474216f8d9effba99fbbd2a99
territoriovivo <territorializacaotaynara@gmail.com>
2026-08-29 13:25:55 -0400

    config: point frontend to restored Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TERRITORIO_VIVO_CONFIG = Object.freeze({
-  supabaseUrl: 'https://wknmlbiqbiissedzrato.supabase.co',
-  supabasePublishableKey: 'sb_publishable_wnlgYYxIJ089tnEk4sUp4w_SYch4EZl',
+  supabaseUrl: 'https://vcgqzdaamwvcwxcuxsmo.supabase.co',
+  supabasePublishableKey: 'sb_publishable_4ZTLflVwYyYbQkxmbQhvlw_wsm3na1E',
   publicUrl: 'https://territoriovivo.github.io/territorio-vivo-carteirinhas/'
 });
